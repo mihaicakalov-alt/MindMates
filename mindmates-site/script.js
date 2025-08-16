354dc71391bbbdfb547313fc6da3b4254de6af39
Mikhail <mihaicakalov-alt@example.com>
2025-08-16 13:20:30 +0300

    отов к деплою: удалил лишний символ

---
+++ mindmates-site/script.js
@@ -0,0 +1,19 @@
+﻿async function loadCharacters() {
+  const res = await fetch('characters.json');
+  const characters = await res.json();
+  const container = document.getElementById('characters-container');
+
+  characters.forEach(c => {
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML = 
+      <img src="\" alt="\">
+      <h2>\</h2>
+      <p>\</p>
+      <a href="chat.html" class="chat-btn">Чат</a>
+    ;
+    container.appendChild(card);
+  });
+}
+
+loadCharacters();
